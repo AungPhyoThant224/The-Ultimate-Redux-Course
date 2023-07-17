aecd1f679953e997a2ea5f79025f3d3268d4a2e8
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-17 11:57:52 +0630

    Composing and Piping.

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,7 +1,13 @@
+import { compose, pipe } from "lodash/fp"
+
 let input = " JavaScript ";
 
 const trim = str => str.trim();
 const wrapInDiv = str => `<div>${str}</div>`
 const toLowerCase = str => str.toLowerCase();
 
-console.log(wrapInDiv(toLowerCase(trim(input))));
+let transformC = compose(wrapInDiv, toLowerCase, trim);
+console.log(transformC(input));
+
+let transformP = pipe(trim, toLowerCase, wrapInDiv);
+console.log(transformP(input));

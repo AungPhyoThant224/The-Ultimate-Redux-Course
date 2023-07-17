65a1bf8de4086218a1b425b6962c6d479bd6f515
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-17 11:54:14 +0630

    Function Composition.

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+let input = " JavaScript ";
+
+const trim = str => str.trim();
+const wrapInDiv = str => `<div>${str}</div>`
+const toLowerCase = str => str.toLowerCase();
+
+console.log(wrapInDiv(toLowerCase(trim(input))));

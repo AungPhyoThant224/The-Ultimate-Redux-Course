46bd7a46dd98ec817469fb8ab00f7e26a42a7cd3
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-18 13:41:33 +0630

    Ducks Pattern.

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,5 +1,7 @@
-import { bugAdded, bugRemoved, bugResolved } from "./actions";
-import store from "./store";
+import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+import configureStore from "./store/configureStore";
+
+const store = configureStore();
 
 const unsubscribe = store.subscribe(() => console.log('Store Change!', store.getState()));
 

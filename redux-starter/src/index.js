f7bcd158575c334396eef28fa37421ea11ed79dd
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-18 11:02:20 +0630

    Exercise: Implementing Resolved Funcition.

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,12 +1,12 @@
-import { bugAdded, bugRemoved } from "./actions";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
 import store from "./store";
 
 const unsubscribe = store.subscribe(() => console.log('Store Change!', store.getState()));
 
 store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugAdded("Bug 2"));
+store.dispatch(bugResolved(1));
+// store.dispatch(bugRemoved(1));
 
 unsubscribe();
-
-store.dispatch(bugRemoved(1))
-
 console.log(store.getState());

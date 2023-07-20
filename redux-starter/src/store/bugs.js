2f9c369b7185d049ce10a272a221c194b2b08c36
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-20 09:43:00 +0630

    Creating Reducer.

--- redux-starter/src/store/bugs.js
+++ redux-starter/src/store/bugs.js
@@ -1,4 +1,4 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 export const bugAdded = createAction("bugAdded");
 export const bugRemoved = createAction("bugRemoved");
@@ -7,22 +7,16 @@ export const bugResolved = createAction("bugResolved");
 //Reducers
 let lastId = 0;
 
-export default function reducer(state = [], action) {
-    switch (action.type) {
-        case bugAdded.type:
-            return [
-                ...state,
-                {
-                    id: ++lastId,
-                    description: action.payload.description,
-                    resolved: false,
-                }
-            ]
-        case bugRemoved.type:
-            return state.filter(bug => bug.id !== action.payload.id);
-        case bugResolved.type:
-            return state.map(bug => (bug.id === action.payload.id) ? { ...bug, resolved: true } : bug)
-        default:
-            return state;
+export default createReducer([], {
+    [bugAdded.type]: (state, action) => {
+        state.push({
+            id: ++lastId,
+            description: action.payload.description,
+            resolved: false,
+        })
+    },
+    [bugResolved.type]: (bugs, action) => {
+        const index = bugs.findIndex(bug => bug.id === action.payload.id);
+        bugs[index].resolved = true;
     }
-}
+})

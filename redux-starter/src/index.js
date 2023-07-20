d78b02108d9b46178120f9048a5bcc30f15d6fcd
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-20 10:20:08 +0630

    Creating Slice and Exercise.

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,14 +1,22 @@
-import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+// import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+// import configureStore from "./store/configureStore";
+
+// const store = configureStore();
+
+// const unsubscribe = store.subscribe(() => console.log('Store Change!', store.getState()));
+
+// store.dispatch(bugAdded({ descripton: "Bug 1" }));
+// store.dispatch(bugAdded({ descripton: "Bug 2" }));
+// store.dispatch(bugResolved({ id: 1 }));
+// // store.dispatch(bugRemoved(1));
+
+// unsubscribe();
+// console.log(store.getState());
+
+import { projectAdded } from "./store/project";
 import configureStore from "./store/configureStore";
 
 const store = configureStore();
 
-const unsubscribe = store.subscribe(() => console.log('Store Change!', store.getState()));
-
-store.dispatch(bugAdded({ descripton: "Bug 1" }));
-store.dispatch(bugAdded({ descripton: "Bug 2" }));
-store.dispatch(bugResolved({ id: 1 }));
-// store.dispatch(bugRemoved(1));
-
-unsubscribe();
-console.log(store.getState());
+store.dispatch(projectAdded({ name: "Project 1" }));
+store.dispatch(projectAdded({ name: "Project 2" }));

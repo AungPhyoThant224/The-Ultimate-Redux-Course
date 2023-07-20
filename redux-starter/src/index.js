5f0114a7011bbae95d98a51552326ad94145d5d4
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-20 14:53:44 +0630

    Exercise.

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -23,5 +23,6 @@ const store = configureStore();
 
 store.dispatch((dispatch, getState) => {
     dispatch({ type: 'bugReceived', bugs: [1, 2, 3] });
+    dispatch({ type: 'error', payload: { message: 'An error occured.' } })
     console.log(getState());
 });

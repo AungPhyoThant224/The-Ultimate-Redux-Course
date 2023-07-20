f2fd05293f4685a8983d3ec92a5b5b01a88030a1
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-20 14:02:49 +0630

    Creating a Middleware.

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -9,14 +9,14 @@ const unsubscribe = store.subscribe(() => console.log('Store Change!', store.get
 
 
 store.dispatch(userAdded({ name: "ABC" }));
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(projectAdded({ name: "Project 2" }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugResolved({ id: 1 }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-const unresolvedBugs = getUnresolvedBugs(store.getState());
+// store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(projectAdded({ name: "Project 2" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
 
+const unresolvedBugs = getUnresolvedBugs(store.getState());
 const bugByUser = getBugByUser(1)(store.getState());
 
 unsubscribe();

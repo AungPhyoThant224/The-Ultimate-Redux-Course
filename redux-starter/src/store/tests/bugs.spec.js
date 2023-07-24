39120361ea1c90fa01c67a5718f93e13071a3ca3
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-24 15:32:06 +0630

    Mocking HTTP Calls.

--- redux-starter/src/store/tests/bugs.spec.js
+++ redux-starter/src/store/tests/bugs.spec.js
@@ -1,13 +1,20 @@
 // Social Test.
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import { addBug } from "../bugs";
 import configureStore from "../configureStore";
 
 describe("Bug Slice", () => {
     it("should handle addBug action", async () => {
         const bug = { description: 'a' };
+        const saveBug = { ...bug, id: 1 };
+
+        const fakeAxios = new MockAdapter(axios);
+        fakeAxios.onPost('/bugs').reply(200, saveBug);
+
         const store = configureStore();
         await store.dispatch(addBug(bug));
-        expect(store.getState().entities.bugs.list).toHaveLength(1);
+        expect(store.getState().entities.bugs.list).toContainEqual(saveBug);
     })
 })
 
